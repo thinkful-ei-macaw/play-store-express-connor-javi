8b676f77a6f8c1516cce9865411652953829f598
Connor McCormack <mccormack.connor.m@gmail.com>
2020-03-24 12:03:58 -0500

    working!

--- app.js
+++ app.js
@@ -9,11 +9,37 @@ const appList  = require('./playstore.js');
 
 app.get('/apps', (req, res) => {
 
-  const { genres, sort } = req.query;
+  const { genres = '', sort = ''} = req.query;
 
+  if (sort) {
+    if (!['Rating', 'App'].includes(sort)) {
+      return res
+        .status(400)
+        .send('Sort must be either rating or app');
+    }
+  }
+
+  let updatedApps = [...appList];
+
+  if (genres) {
+    updatedApps = updatedApps
+    .filter(app =>
+      app
+        .Genres
+        .toLowerCase()
+        .includes(genres.toLowerCase()));
+  }
+
+  // a["app"], a = {App: "ROBLOX"}, b = {App: "Subway Surfers"}, "ROBLOX" < "Subway Surfers" ?
+  if (sort) {
+    updatedApps
+      .sort((a, b) => {
+        return a[sort] > b[sort] ? 1 : a[sort] < b[sort] ? -1 : 0;
+    });
+  }
+
+  res.json(updatedApps);
   
-
-  res.json(appList)
 
 });
 
